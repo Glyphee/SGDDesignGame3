e3cbd4ba060be3e6dca4690b1ce4930654712472
Glyphee <77804365+Glyphee@users.noreply.github.com>
2021-10-30 15:04:11 -0400

    created the DetectClosest function

--- SGDDesignGame3/Assets/Scripts/PlayerCon.cs
+++ SGDDesignGame3/Assets/Scripts/PlayerCon.cs
@@ -7,13 +7,14 @@ public class PlayerCon : MonoBehaviour
     [Header("Movement values")]
     Vector2 move;
     [SerializeField] float speed = 0;
-    [SerializeField] float accelerationTime = 2;
+    [SerializeField] private GameObject[] objects = new GameObject[0];
+    [SerializeField] private int possessedObject;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -30,6 +31,23 @@ public class PlayerCon : MonoBehaviour
         transform.Translate(move * speed * Time.deltaTime);
     }
 
-    //possession mechanic to follow
-
+    GameObject DetectClosest()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (Vector2.Distance(this.gameObject.transform.position, objects[i].transform.position) < 1)
+            {
+                int closestObject = 0;
+                for (int k = 0; k < objects.Length; k++)
+                {
+                    if (Vector2.Distance(this.gameObject.transform.position, objects[k].transform.position) < Vector2.Distance(this.gameObject.transform.position, objects[closestObject].transform.position))
+                    {
+                        closestObject = k;
+                    }
+                }
+                return objects[closestObject];
+            }
+        }
+        return null;
+    }
 }
