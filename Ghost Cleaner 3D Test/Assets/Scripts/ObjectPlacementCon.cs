00f12977fcce1d200954c45298a43731fd7976da
TalynE <64740653+TalynE@users.noreply.github.com>
2021-10-31 17:58:21 -0400

    Furthered Cleaning Arrangements
    Player can now place three objects and have the game recognize it; to be expanded on later

--- Ghost Cleaner 3D Test/Assets/Scripts/ObjectPlacementCon.cs
+++ Ghost Cleaner 3D Test/Assets/Scripts/ObjectPlacementCon.cs
@@ -5,25 +5,54 @@ using UnityEngine;
 public class ObjectPlacementCon : MonoBehaviour
 {
     [Header("Level Objects")]
-    [SerializeField] GameObject[] objectsToPlace;
-    [SerializeField] GameObject[] ghostObjects;
+    [SerializeField] GameObject[] objectsToPlace = new GameObject[3];
+    [SerializeField] GameObject[] ghostObjects = new GameObject[3];
+
+    float stoppedTime;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(DoPlacementCheck());
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlacementCheck();
+        
     }
 
     //the following should check if objects are placed in the correct location, and if so, freeze the timer and provide a next level/menu option
     void PlacementCheck()
     {
-               
+        int placed = 0;
+        foreach (GameObject o in objectsToPlace)
+        {
+            foreach(GameObject g in ghostObjects)
+            {
+                if(Vector3.Distance(o.transform.position, g.transform.position) < 1f && o.transform.parent == null)
+                {
+                    o.transform.position = g.transform.position;
+                    o.transform.rotation = g.transform.rotation;
+                    placed++;
+                    print("Objects in place: " + placed.ToString());
+                }
+            }
+        }
+
+        if(placed == ghostObjects.Length)
+        {
+            print("You did it!");
+        }
+    }
+
+    IEnumerator DoPlacementCheck()
+    {
+        while (true)
+        {
+            PlacementCheck();
+            yield return new WaitForSeconds(.2f);
+        }        
     }
 }
