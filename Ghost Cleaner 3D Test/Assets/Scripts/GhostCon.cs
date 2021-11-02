c3a7cfe0770d4677cce5f8e5926047bfb139f58e
LoganPomper <91286925+LoganPomper@users.noreply.github.com>
2021-11-02 12:26:06 -0400

    Camera fixes and props
    Fixed camera culling objects and added props for puzzle

--- Ghost Cleaner 3D Test/Assets/Scripts/GhostCon.cs
+++ Ghost Cleaner 3D Test/Assets/Scripts/GhostCon.cs
@@ -31,7 +31,7 @@ public class GhostCon : MonoBehaviour
 
         //cam and movement values
         cam = GetComponentInChildren<Camera>().transform;
-        cam.position = camPosition.position; cam.rotation = Quaternion.Euler(35, -31, -4);
+        cam.position = camPosition.position; cam.rotation = Quaternion.Euler(35, -45, 0);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         canGo = true; //canLook = true;
